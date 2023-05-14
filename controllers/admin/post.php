cba373e5a29e3e9d9781eeebe3d2f9eb54096738
Your Name <you@example.com>
2023-05-15 00:07:17 +0700

    fix

--- controllers/admin/post.php
+++ controllers/admin/post.php
@@ -4,18 +4,10 @@ header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Request-With");
 
-<<<<<<< HEAD
-include_once("../../models/AdminModel.php");
-$admin = new AdminModel();
-$data = json_decode(file_get_contents("php://input"));
-print_r ($admin);
-=======
-
 include_once("../../models/AdminModel.php");
 $admin = new AdminModel();
 $data = json_decode(file_get_contents("php://input"));
 
->>>>>>> 9abddbe71a54e1c6bc5ed6ed0f4023fd6cb630c0
 $admin->adminname = $data->adminname;
 $admin->role = $data->role;
 $admin->password = $data->password;
